fedb34f8e48e894139184abaf2979a83f905dbde
LE_ANH_THUONG\thuon <leanhthuong2k3@gmail.com>
2024-03-12 15:05:23 +0700

    first commit

---
+++ src/main/java/cbd/com/vn/controller/user/HomeController.java
@@ -0,0 +1,17 @@
+package cbd.com.vn.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping()
+    public String view(Model model) {
+        model.addAttribute("value", "hello");
+        return "/user/layouts/home";
+    }
+}
---
+++ src/test/java/cbd/com/vn/VnApplicationTests.java
@@ -0,0 +1,13 @@
+package cbd.com.vn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
