0007d39fc2b4ee731a27b5d3faefe566306aea4d
LE_ANH_THUONG\thuon <leanhthuong2k3@gmail.com>
2024-03-22 21:13:23 +0700

    update security

--- src/main/java/cbd/com/vn/controller/admin/DashBoardController.java
+++ src/main/java/cbd/com/vn/controller/admin/DashBoardController.java
@@ -1,10 +1,14 @@
 package cbd.com.vn.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin/dashboard")
+@RequestMapping("dashboard")
 public class DashBoardController {
-
+    @GetMapping
+    public String view() {
+        return "/admin/index";
+    }
 }
