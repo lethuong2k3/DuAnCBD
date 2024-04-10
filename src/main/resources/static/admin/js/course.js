2578a3f91d12542368a41fbcc92b31ad67fe02b5
LE_ANH_THUONG\thuon <leanhthuong2k3@gmail.com>
2024-04-10 13:05:18 +0700

    fix bug modal page course

--- src/main/resources/static/admin/js/course.js
+++ src/main/resources/static/admin/js/course.js
@@ -1,4 +1,22 @@
 let app_course = angular.module("course", []);
+
 app_course.controller("course-ctrl", function ($scope, $http){
     $scope.fees = [];
+    $scope.addFee = () => {
+        if (
+            $scope.fees.length >= 5
+        ) {
+            toastr["warning"]("Chỉ được thêm tối đa 5 học phí");
+            return;
+        }
+        let fee = {
+            title: $scope.title,
+            description: $scope.description,
+            price: $scope.price
+        }
+        $scope.fees.push(fee);
+        $('#feeModal').modal('hide');
+        $('#courseModal').modal('show');
+
+    }
 })
---
+++ src/main/resources/static/assets/js/login.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+})(jQuery);
+
